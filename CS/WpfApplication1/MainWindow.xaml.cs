50e56ff6875c751ba378a9ca6abaca87119dc25f
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows;
+using DevExpress.Xpf.Gauges;
+
+namespace WpfApplication1 {
+
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void OnCreepingLineAnimationCompleted(object sender, EventArgs e) {
+            BlinkingAnimation blinking = new BlinkingAnimation();
+            matrix.Animation = blinking;
+            digitalGaugeControl1.Text = "www.devexpress.com";
+        }
+
+    }
+}
---
+++ VB/WpfApplication1/MainWindow.xaml.vb
@@ -0,0 +1,21 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Windows
+Imports DevExpress.Xpf.Gauges
+
+Namespace WpfApplication1
+
+	Partial Public Class MainWindow
+		Inherits Window
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub OnCreepingLineAnimationCompleted(ByVal sender As Object, ByVal e As EventArgs)
+			Dim blinking As New BlinkingAnimation()
+			matrix.Animation = blinking
+			digitalGaugeControl1.Text = "www.devexpress.com"
+		End Sub
+
+	End Class
+End Namespace
